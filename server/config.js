b0d1cc533517b655dc18f1575714c3f8c60119b2
Niklas Rasmusson <niklas.rasmusson@gmail.com>
2013-07-15 11:28:40 +0200

    Change: port

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = {
   server: {
-    listenPort: 3000,        // The port on which the server is to listen (means that the app is at http://localhost:3000 for instance)
+    listenPort: 8080,        // The port on which the server is to listen (means that the app is at http://localhost:3000 for instance)
     clientRoot: '/../client' // the place from where we will serve our static resources
   }
 };
