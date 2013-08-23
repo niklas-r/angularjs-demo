75737959d63371e8c3a12a2d7f42d77c6e3bca80
Niklas Rasmusson <niklas.rasmusson@gmail.com>
2013-08-23 09:20:10 +0200

    Change/add: more tests

--- client/test/unit/common/services/pagesSpec.js
+++ client/test/unit/common/services/pagesSpec.js
@@ -1,23 +1,43 @@
 describe("Page service", function() {
-  var pageService, location;
+  var pageService;
 
-  // load the controller's module
   beforeEach(module('services.pages'));
 
-  beforeEach(inject(function(pages, $location) {
-    pageService = pages;
-    location = $location;
-  }));
+  beforeEach(function() {
 
-  it("should have an active page.", function () {
-    var activePage;
-    location.path("/");
+    inject(function ($injector) {
+      pageService = $injector.get('pages');
+    });
 
-    pageService.setActivePage(location);
-    activePage = pageService.getActivePage();
-
-    expect(activePage.title).toBeTruthy();
   });
 
-  
+  it("should get all pages", function () {
+    var pages = pageService.getPages();
+    expect(pages).toEqual(jasmine.any(Array));
+  });
+
+  it("should get specific page", function () {
+    var page = pageService.getPageById(0);
+
+    expect(page.title).toEqual("Hem");
+  });
+
+  it("should set/get active page", inject(function ($location) {
+    var activePage;
+
+    $location.path("/");
+    setActivePage = pageService.setActivePage($location);
+
+    expect(setActivePage.title).toEqual("Hem");
+  }));
+
+  it("should get active page", inject(function ($location) {
+    $location.path("/");
+
+    pageService.setActivePage($location);
+    
+    getActivePage = pageService.getActivePage();
+
+    expect(getActivePage.title).toEqual("Hem");
+  }));
 });
